8df75efd39f1953fe62031defe5421e91b1946dc
Bill Leung <bill8575@users.noreply.github.com>
2022-02-09 21:36:47 -0500

    Add port variable

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.get("/", (req, res) => {
   res.send("I am home page, man!!")
 });
 
-app.listen(5000, () => {
+port = process.env.port || 5000
+app.listen(port, () => {
   console.log("Hey, man, Wazzapp");
 });
 
